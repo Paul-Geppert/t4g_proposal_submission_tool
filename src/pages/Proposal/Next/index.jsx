2ec6c8619a8b1e81ea10414a43a5f0d3915bd9eb
Paul Geppert <paul.geppert@student.hpi.de>
2021-10-29 10:44:55 +0200

    feat(): Improved next steps

--- src/pages/Proposal/Next/index.jsx
+++ src/pages/Proposal/Next/index.jsx
@@ -1,6 +1,7 @@
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import React from 'react';
 
+import Link from '../../../components/Link';
 import Page from '../../../components/Page';
 
 const ProposalNext = () => (
@@ -9,20 +10,37 @@ const ProposalNext = () => (
     description="What you should do next."
     className="ProposalNext"
   >
-    <Button
-      className="PDFDownload"
-      href="http://localhost:3003/"
-      download
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={4}
+      alignItems="center"
     >
-      Download PDF
-    </Button>
-    <Button
-      className="XMLDownload"
-      href="http://localhost:3003/xml/"
-      download
-    >
-      Download XML
-    </Button>
+      <Link
+        externalTarget="https://foerderportal.bund.de/easyonline/"
+        target="_blank"
+      >
+        Zu easy-Online
+      </Link>
+      <Button
+        className="PDFDownload"
+        variant="outlined"
+        href="http://localhost:3003/"
+        sx={{ width: '50%' }}
+        download
+      >
+        Download PDF
+      </Button>
+      <Button
+        className="XMLDownload"
+        variant="outlined"
+        href="http://localhost:3003/xml/"
+        sx={{ width: '50%' }}
+        download
+      >
+        Download XML
+      </Button>
+    </Box>
   </Page>
 );
 
