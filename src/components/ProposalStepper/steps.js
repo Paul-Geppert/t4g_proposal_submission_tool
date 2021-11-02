d14130576d7b17068ce0b0bc9a1c19a0b08f451b
Paul Geppert <paul.geppert@student.hpi.de>
2021-11-02 10:35:02 +0100

    feat(): Better example texts

--- src/components/ProposalStepper/steps.js
+++ src/components/ProposalStepper/steps.js
@@ -34,7 +34,7 @@ Im Anschluss soll das Flugtaxi im privaten **öffentlichen Nahverkehr** angebote
         type: 'markdown',
         exampleAnswer: `Flugtaxis (siehe Abbildung 1) bieten eine neuartige Fortbewegungsart.
 
-![Abbildung eines Flutaxis](https://www.golem.de/1905/141054-195064-195063_rc.jpg)
+![Abbildung eines Flugtaxis](https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Volocopter%2CIAA_2017%2C_Frankfurt_%281Y7A1911%29.jpg/800px-Volocopter%2CIAA_2017%2C_Frankfurt_%281Y7A1911%29.jpg)
 Abbildung 1
 
 Anders als Flugzeuge, handelt es sich um für ein Verkehrsmittel, das für die breite Masse der Bevölkerung gedacht ist.
@@ -51,22 +51,48 @@ Damit die Verkehrs- und Klimawende erfolgreich umgesetzt wird, ist das entwickel
       {
         title: 'Einführung, Motivation, Ausgangssituation, ggf. Beispielszenario',
         type: 'markdown',
-        exampleAnswer: 'd',
+        exampleAnswer: `Ein umweltfreundliches Lufttaxi löst die meisten der aktuellen Verkehrsprobleme, wie in der Tabelle unten zu sehen ist:
+
+Problem | Lösung
+---- | ----
+Klimakriese | Wasserstoffantrieb
+Verstopfte Straßen | Nutzung Luftraum
+Straßenbau | Luftraum ist wartungsfrei
+
+Auch Tech4Germany-Fellows bekunden Interesse:
+
+> "Ich hätte auch gern so ein Flugtaxi für mich und meine Familie" (Paul)`,
       },
       {
         title: 'Kernziel des Vorhabens und Projektidee',
         type: 'markdown',
-        exampleAnswer: 'e',
+        exampleAnswer: `Wir entwicklen ein umweltfreundliches, *autonomes* Flugtaxi.
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Passenger_Drone.jpg/800px-Passenger_Drone.jpg)`,
       },
       {
         title: 'Erläuterung des Bezugs des Vorhabens zum Förderaufruf ',
         type: 'markdown',
-        exampleAnswer: 'f',
+        exampleAnswer: `Wie im Förderaufruf gefordert, möchten wir ein neuartiges Fortbewegungsmittel für den Luftraum entwickeln.
+
+Dazu ist Grundlagenforschung nötig, insbesondere:
+- die autonome Steuerung eines Helikopters
+- kooperative Verkehrssituationen im Luftraum
+- sicherer Transport von Menschen
+- Einsatzmöglichkeit von Wasserstoffantrieben für Helikopter `,
       },
       {
         title: 'Abgrenzung und Alleinstellungsmerkmal gegenüber inhaltlich ähnlich gelagerten UAS & Flugtaxis-Förderprojekten',
         type: 'markdown',
-        exampleAnswer: 'g',
+        exampleAnswer: `Andere Projekte und der aktuelle Stand der Technik fokussiert sich auf Diesel- und Elektromotoren mit Akkuspeicher.
+
+Mit unserem Ansatz des Wasserstoffantriebs kann eine umweltfreundliche Alternative geschaffen werden.
+
+--------
+
+Hier ist eine Abgrenzung von der Nutzung von Wasserstoff für verschiedene Antriebsarten.
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Einsatzbereiche_sauberen_Wasserstoff.png/800px-Einsatzbereiche_sauberen_Wasserstoff.png)`,
       },
     ],
   },
