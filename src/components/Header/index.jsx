0749c3f478c9662a0b48b73240145a6516d84196
Paul Geppert <paul.geppert@student.hpi.de>
2021-11-02 11:15:05 +0100

    feat(): Image for Anna

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import {
   AppBar, Box, Toolbar, Typography,
 } from '@mui/material';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import React from 'react';
 
 import AppIcon from '../AppIcon';
+
+import contactImage from '../../image/anstragstellerin.png';
 
 const Header = () => (
   <Box sx={{ flexGrow: 1, my: 3 }} className="ProposalSideHeader">
@@ -25,8 +26,8 @@ const Header = () => (
         />
         <Box sx={{ flexGrow: 1 }} />
         <Box display="flex" alignItems="center">
-          <Typography style={{ fontFamily: 'BundessansBold' }}>Anna Antragstellerin</Typography>
-          <AccountCircleOutlinedIcon />
+          <Typography style={{ fontFamily: 'BundessansBold' }} sx={{ mr: 3 }}>Anna Antragstellerin</Typography>
+          <img src={contactImage} alt="Bild von Ansprechpartner:in" height={50} />
         </Box>
       </Toolbar>
     </AppBar>
