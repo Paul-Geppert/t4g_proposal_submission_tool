1229dec657e90db4f1648b0006793f7b6f89b2fc
Paul Geppert <paul.geppert@student.hpi.de>
2021-11-02 10:08:53 +0100

    feat(): Higher markdown questions

--- src/components/ProposalStepper/components/Questions/MarkdownQuestion/index.jsx
+++ src/components/ProposalStepper/components/Questions/MarkdownQuestion/index.jsx
@@ -16,6 +16,7 @@ const MarkdownQuestion = ({
           setShowError(true);
           onChange(value);
         }}
+        height={350}
       />
       {showError && !validated && (
         <Typography
